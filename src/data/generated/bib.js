67af3ec231460ec5a18151cf533ab4675c0184cd
Jessie-yi <may13466680960@163.com>
2025-05-16 17:41:27 +0100

    Add files via upload

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,18 @@
-const generatedBibEntries = {
+﻿const generatedBibEntries = {
     "barth2024": {
-        "author": "C.-M. Barth and J. Bernard and E. M. Huang",
+        "author": "Barth, Clara-Maria, Bernard, J\u00fcrgen and Huang, Elaine~May",
         "booktitle": "Proceedings of the ACM on Human-Computer Interaction (CSCW)",
         "doi": "10.1145/3613904.3642234",
+        "number": "1",
+        "pages": "1--10",
+        "publisher": "ACM",
         "title": "Exploring the Role of Blood Glucose Prediction Technologies for Type 1 Diabetes Self-Management",
         "type": "inproceedings",
-        "year": "2024",
-        "number": "1",
         "volume": "1",
-        "pages": "1--10"
+        "year": "2024"
     },
     "dinu2017": {
-        "author": "M. Dinu and R. Abbate and G. F. Gensini and A. Casini and F. Sofi",
+        "author": "Dinu, Monica and Abbate, Rosanna and Gensini, Gian~Franco and Casini, Alessandro and Sofi, Francesco",
         "doi": "10.1080/10408398.2016.1138447",
         "journal": "Critical Reviews in Food Science and Nutrition",
         "number": "17",
@@ -22,7 +23,7 @@ const generatedBibEntries = {
         "year": "2017"
     },
     "esposito2009": {
-        "author": "K. Esposito and et al.",
+        "author": "Esposito, Katherine and Maiorino, Maria~Ida and Ciotola, Miryam and {Di~Palo Carmen and Scognamiglio, Paola and Gicchino, Maurizio and Petrizzo, Michela and Saccomanno, Franco and Beneduce, Flora and Ceriello, Antonio and Giugliano, Dario",
         "doi": "10.7326/0003-4819-151-5-200909010-00004",
         "journal": "Annals of Internal Medicine",
         "number": "5",
@@ -33,7 +34,7 @@ const generatedBibEntries = {
         "year": "2009"
     },
     "liu2022": {
-        "author": "X. Liu and M. Alharbi and J. Chen and A. Diehl and E. E. Firat and D. Rees and Q. Wang and R. S. Laramee",
+        "author": "Liu, Xiaoxiao and Alharbi, Mohammad and Chen, Jian and Diehl, Alexandra and Rees, Dylan and Firat, Elif and Wang, Qiru and Laramee, Robert",
         "doi": "10.1177/14738716221126992",
         "journal": "Information Visualization",
         "number": "1",
@@ -44,29 +45,29 @@ const generatedBibEntries = {
         "year": "2022"
     },
     "mcnabb2017": {
-        "author": "L. McNabb and R. S. Laramee",
+        "author": "McNabb, Liam and Laramee, Robert",
         "doi": "10.1111/cgf.13212",
         "journal": "Computer Graphics Forum",
         "number": "3",
         "pages": "589--612",
-        "title": "Survey of Surveys (SoS)",
+        "title": "Survey of Surveys ({SoS})",
         "type": "article",
         "volume": "36",
         "year": "2017"
     },
     "miotto2016": {
-        "author": "R. Miotto and L. Li and B. A. Kidd and J. T. Dudley",
+        "author": "Miotto, Riccardo and Li, Li and Kidd, Brian Andrew and Dudley, Joel Thomas",
         "doi": "10.1038/srep26094",
         "journal": "Scientific Reports",
+        "number": "1",
         "pages": "26094",
         "title": "Deep Patient: An Unsupervised Representation to Predict the Future of Patients from the Electronic Health Records",
         "type": "article",
         "volume": "6",
-        "year": "2016",
-        "number": "1"
+        "year": "2016"
     },
     "rees2019": {
-        "author": "D. Rees and R. S. Laramee",
+        "author": "Rees, Dylan and Laramee, Robert",
         "doi": "10.1111/cgf.13595",
         "journal": "Computer Graphics Forum",
         "number": "3",
@@ -77,34 +78,35 @@ const generatedBibEntries = {
         "year": "2019"
     },
     "scimone2024": {
-        "author": "A. Scimone and K. Eckelt and M. Streit and A. Hinterreiter",
+        "author": "Scimone, Anna and Eckelt, Klaus and Streit, Marc and Hinterreiter, Andreas",
         "doi": "10.1109/TVCG.2023.3326936",
         "journal": "IEEE Transactions on Visualization and Computer Graphics",
+        "number": "1",
+        "pages": "1--10",
         "title": "Marjorie: Visualizing Type 1 Diabetes Data to Support Pattern Exploration",
         "type": "article",
-        "year": "2024",
-        "number": "1",
         "volume": "1",
-        "pages": "1--10"
+        "year": "2024"
     },
     "tong2017": {
-        "author": "C. Tong and R. Roberts and R. S. Laramee and D. Thayer and D. Berridge",
-        "booktitle": "Computer Graphics and Visual Computing (CGVC)",
+        "author": "Tong, Chao and Roberts, Richard and Laramee, Robert and Berridge, Damon and Thayer, Daniel",
+        "booktitle": "Computer Graphics and Visual Computing (CGVC) 2017",
         "doi": "10.2312/cgvc.20171276",
-        "pages": "61--70",
+        "editor": "Wan, Tao Ruan and Vidal, Franck",
+        "isbn": "978-3-03868-050-5",
+        "pages": "29--42",
+        "publisher": "The Eurographics Association",
         "title": "Cartographic Treemaps for the Visualization of Public Healthcare Data",
         "type": "inproceedings",
-        "year": "2017",
-        "number": "1",
-        "volume": "1"
+        "year": "2017"
     },
     "wang2022": {
-        "author": "Q. Wang and R. S. Laramee",
+        "author": "Wang, Qiru and Laramee, Robert",
         "doi": "10.1111/cgf.14424",
         "journal": "Computer Graphics Forum",
         "number": "3",
         "pages": "487--510",
-        "title": "EHR STAR: The State-of-the-Art in Interactive EHR Visualization",
+        "title": "EHR {STAR}: The State-of-the-Art in Interactive {EHR} Visualization",
         "type": "article",
         "volume": "41",
         "year": "2022"
