9745a4705563b865ff648d52a288df7eb53114b6
Jessie-yi <may13466680960@163.com>
2025-05-16 11:38:42 +0100

    Add files via upload

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -3,8 +3,11 @@
         "author": "X. Liu and M. Alharbi and J. Chen and A. Diehl and E. E. Firat and D. Rees and Q. Wang and R. S. Laramee",
         "doi": "10.1177/14738716221126992",
         "journal": "Information Visualization",
+        "number": "1",
+        "pages": "3--30",
         "title": "Visualization Resources: A Survey",
         "type": "article",
+        "volume": "22",
         "year": "2022"
     },
     "rees2019": {
