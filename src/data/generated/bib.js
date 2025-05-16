08d2b583fb9f6d2e16d28afd7b077e7ed539a514
Jessie-yi <may13466680960@163.com>
2025-05-16 21:07:39 +0100

    Add files via upload

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -7,10 +7,16 @@
         "pages": "1--10",
         "publisher": "ACM",
         "title": "Exploring the Role of Blood Glucose Prediction Technologies for Type 1 Diabetes Self-Management",
-        "type": "inproceedings",
+        "type": "UX Study + Prediction System",
         "volume": "1",
         "year": "2024"
-    },
+        "tags": [
+        "prediction",
+        "diabetes",
+        "interface",
+        "UX"
+    ]
+  },
     "dinu2017": {
         "author": "Dinu, Monica and Abbate, Rosanna and Gensini, Gian~Franco and Casini, Alessandro and Sofi, Francesco",
         "doi": "10.1080/10408398.2016.1138447",
@@ -18,10 +24,15 @@
         "number": "17",
         "pages": "3640--3649",
         "title": "Vegetarian, Vegan Diets and Multiple Health Outcomes: A Systematic Review with Meta-analysis of Observational Studies",
-        "type": "article",
+        "type": "Meta-analysis",
         "volume": "57",
-        "year": "2017"
-    },
+        "year": "2017",
+        "tags": [
+        "diet",
+        "vegetarian",
+        "health outcomes"
+    ]
+  },
     "esposito2009": {
         "author": "Esposito, Katherine and Maiorino, Maria~Ida and Ciotola, Miryam and {Di~Palo Carmen and Scognamiglio, Paola and Gicchino, Maurizio and Petrizzo, Michela and Saccomanno, Franco and Beneduce, Flora and Ceriello, Antonio and Giugliano, Dario",
         "doi": "10.7326/0003-4819-151-5-200909010-00004",
@@ -29,10 +40,15 @@
         "number": "5",
         "pages": "306--314",
         "title": "Effects of a Mediterranean-style Diet on the Need for Antihyperglycemic Drug Therapy in Patients with Newly Diagnosed Type 2 Diabetes",
-        "type": "article",
+        "type": "RCT",
         "volume": "151",
-        "year": "2009"
-    },
+        "year": "2009",
+        "tags": [
+        "diet",
+        "Mediterranean",
+        "intervention"
+    ]
+  },
     "liu2022": {
         "author": "Liu, Xiaoxiao and Alharbi, Mohammad and Chen, Jian and Diehl, Alexandra and Rees, Dylan and Firat, Elif and Wang, Qiru and Laramee, Robert",
         "doi": "10.1177/14738716221126992",
@@ -40,10 +56,15 @@
         "number": "1",
         "pages": "3--30",
         "title": "Visualization Resources: A Survey",
-        "type": "article",
+        "type": "Survey",
         "volume": "22",
-        "year": "2022"
-    },
+        "year": "2022",
+        "tags": [
+        "classification",
+        "taxonomy",
+        "resources"
+    ]
+  },
     "mcnabb2017": {
         "author": "McNabb, Liam and Laramee, Robert",
         "doi": "10.1111/cgf.13212",
@@ -51,10 +72,15 @@
         "number": "3",
         "pages": "589--612",
         "title": "Survey of Surveys ({SoS})",
-        "type": "article",
+        "type": "Meta-analysis",
         "volume": "36",
-        "year": "2017"
-    },
+        "year": "2017",
+        "tags": [
+        "meta-review",
+        "survey of surveys",
+        "categorization"
+    ]
+  },
     "miotto2016": {
         "author": "Miotto, Riccardo and Li, Li and Kidd, Brian Andrew and Dudley, Joel Thomas",
         "doi": "10.1038/srep26094",
@@ -62,10 +88,15 @@
         "number": "1",
         "pages": "26094",
         "title": "Deep Patient: An Unsupervised Representation to Predict the Future of Patients from the Electronic Health Records",
-        "type": "article",
+        "type": "System",
         "volume": "6",
-        "year": "2016"
-    },
+        "year": "2016",
+        "tags": [
+        "EHR",
+        "prediction",
+        "autoencoder"
+    ]
+  },
     "rees2019": {
         "author": "Rees, Dylan and Laramee, Robert",
         "doi": "10.1111/cgf.13595",
@@ -73,10 +104,14 @@
         "number": "3",
         "pages": "561--581",
         "title": "A Survey of Information Visualization Books",
-        "type": "article",
+        "type": "Survey",
         "volume": "38",
-        "year": "2019"
-    },
+        "year": "2019",
+        "tags": [
+        "classification",
+        "resources"
+    ]
+  },
     "scimone2024": {
         "author": "Scimone, Anna and Eckelt, Klaus and Streit, Marc and Hinterreiter, Andreas",
         "doi": "10.1109/TVCG.2023.3326936",
@@ -84,10 +119,15 @@
         "number": "1",
         "pages": "1--10",
         "title": "Marjorie: Visualizing Type 1 Diabetes Data to Support Pattern Exploration",
-        "type": "article",
+        "type": "System",
         "volume": "1",
-        "year": "2024"
-    },
+        "year": "2024",
+        "tags": [
+        "glucose",
+        "diabetes",
+        "timeline"
+    ]
+  },
     "tong2017": {
         "author": "Tong, Chao and Roberts, Richard and Laramee, Robert and Berridge, Damon and Thayer, Daniel",
         "booktitle": "Computer Graphics and Visual Computing (CGVC) 2017",
@@ -97,9 +137,15 @@
         "pages": "29--42",
         "publisher": "The Eurographics Association",
         "title": "Cartographic Treemaps for the Visualization of Public Healthcare Data",
-        "type": "inproceedings",
-        "year": "2017"
-    },
+        "type": "Application",
+        "year": "2017",
+        "tags": [
+        "public health",
+        "map",
+        "treemap",
+        "geographic"
+    ]
+  },
     "wang2022": {
         "author": "Wang, Qiru and Laramee, Robert",
         "doi": "10.1111/cgf.14424",
@@ -107,8 +153,13 @@
         "number": "3",
         "pages": "487--510",
         "title": "EHR {STAR}: The State-of-the-Art in Interactive {EHR} Visualization",
-        "type": "article",
+        "type": "Technical Survey",
         "volume": "41",
-        "year": "2022"
-    }
+        "year": "2022",
+        "tags": [
+        "EHR",
+        "classification",
+        "visual encoding"
+    ]
+  }
 };
