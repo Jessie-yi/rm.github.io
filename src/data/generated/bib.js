0703bf5c342bae04d22419b3ddcd73472f8854f7
Jessie-yi <may13466680960@163.com>
2025-05-16 11:19:28 +0100

    Add files via upload

---
+++ src/data/generated/bib.js
@@ -0,0 +1,18 @@
+﻿const generatedBibEntries = {
+    "liu2022": {
+        "author": "X. Liu and M. Alharbi and J. Chen and A. Diehl and E. E. Firat and D. Rees and Q. Wang and R. S. Laramee",
+        "doi": "10.1177/14738716221126992",
+        "journal": "Information Visualization",
+        "title": "Visualization Resources: A Survey",
+        "type": "article",
+        "year": "2022"
+    },
+    "rees2019": {
+        "author": "D. Rees and R. S. Laramee",
+        "doi": "10.1111/cgf.13595",
+        "journal": "Computer Graphics Forum",
+        "title": "A Survey of Information Visualization Books",
+        "type": "article",
+        "year": "2019"
+    }
+};
---
+++ src/data/generated/config.js
@@ -0,0 +1,32 @@
+var config = [
+  {
+    "id": "liu2022",
+    "title": "Visualization Resources: A Survey",
+    "authors": "X. Liu, M. Alharbi, J. Chen, A. Diehl, E. E. Firat, D. Rees, Q. Wang, and R. S. Laramee",
+    "year": "2022",
+    "topic": "Visualization Resources",
+    "method": "Survey",
+    "evaluation": "None",
+    "tags": [
+      "classification",
+      "taxonomy",
+      "resources"
+    ],
+    "doi": "10.1177/14738716221126992"
+  },
+  {
+    "id": "rees2019",
+    "title": "A Survey of Information Visualization Books",
+    "authors": "D. Rees and R. S. Laramee",
+    "year": "2019",
+    "topic": "Visualization Resources",
+    "method": "Survey",
+    "evaluation": "None",
+    "tags": [
+      "book analysis",
+      "coverage",
+      "education"
+    ],
+    "doi": "10.1111/cgf.13595"
+  }
+];
