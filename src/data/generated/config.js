2ee6b7d13ec92756a34671f34bb89cba32d0bb9d
Jessie-yi <may13466680960@163.com>
2025-05-16 12:48:37 +0100

    Add files via upload

--- src/data/generated/config.js
+++ src/data/generated/config.js
@@ -1,32 +1,200 @@
-var config = [
-  {
-    "id": "liu2022",
-    "title": "Visualization Resources: A Survey",
-    "authors": "X. Liu, M. Alharbi, J. Chen, A. Diehl, E. E. Firat, D. Rees, Q. Wang, and R. S. Laramee",
-    "year": "2022",
-    "topic": "Visualization Resources",
-    "method": "Survey",
-    "evaluation": "None",
-    "tags": [
-      "classification",
-      "taxonomy",
-      "resources"
+const config = {
+  "categories": {
+    "type": [
+      "Survey",
+      "Meta-Survey",
+      "Technical Survey",
+      "Interactive System",
+      "Application",
+      "UX Study + Prediction System",
+      "Deep Learning (Autoencoder)",
+      "RCT",
+      "Meta-analysis"
     ],
-    "doi": "10.1177/14738716221126992"
+    "method": [
+      "Survey",
+      "Meta-Survey",
+      "Technical Survey",
+      "Interactive System",
+      "Application",
+      "UX Study + Prediction System",
+      "Deep Learning (Autoencoder)",
+      "RCT",
+      "Meta-analysis"
+    ],
+    "evaluation": [
+      "None",
+      "User Evaluation",
+      "User Study",
+      "Quantitative (ROC, AUC)",
+      "Survival Analysis (Kaplan-Meier)",
+      "Quantitative (Forest Plot)"
+    ]
   },
-  {
-    "id": "rees2019",
-    "title": "A Survey of Information Visualization Books",
-    "authors": "D. Rees and R. S. Laramee",
-    "year": "2019",
-    "topic": "Visualization Resources",
-    "method": "Survey",
-    "evaluation": "None",
-    "tags": [
-      "book analysis",
-      "coverage",
-      "education"
-    ],
-    "doi": "10.1111/cgf.13595"
+  "entries": {
+    "liu2022": {
+      "type": [
+        "Survey"
+      ],
+      "method": [
+        "Survey"
+      ],
+      "evaluation": [
+        "None"
+      ],
+      "tags": [
+        "classification",
+        "taxonomy",
+        "resources"
+      ]
+    },
+    "rees2019": {
+      "type": [
+        "Survey"
+      ],
+      "method": [
+        "Survey"
+      ],
+      "evaluation": [
+        "None"
+      ],
+      "tags": [
+        "book analysis",
+        "coverage",
+        "education"
+      ]
+    },
+    "mcnabb2017": {
+      "type": [
+        "Meta-Survey"
+      ],
+      "method": [
+        "Meta-Survey"
+      ],
+      "evaluation": [
+        "None"
+      ],
+      "tags": [
+        "meta-review",
+        "survey of surveys",
+        "categorization"
+      ]
+    },
+    "wang2022": {
+      "type": [
+        "Technical Survey"
+      ],
+      "method": [
+        "Technical Survey"
+      ],
+      "evaluation": [
+        "None"
+      ],
+      "tags": [
+        "EHR",
+        "classification",
+        "visual encoding"
+      ]
+    },
+    "scimone2024": {
+      "type": [
+        "Interactive System"
+      ],
+      "method": [
+        "Interactive System"
+      ],
+      "evaluation": [
+        "User Evaluation"
+      ],
+      "tags": [
+        "diabetes",
+        "glucose",
+        "timeline",
+        "UI"
+      ]
+    },
+    "tong2017": {
+      "type": [
+        "Application"
+      ],
+      "method": [
+        "Application"
+      ],
+      "evaluation": [
+        "None"
+      ],
+      "tags": [
+        "public health",
+        "map",
+        "treemap",
+        "geographic"
+      ]
+    },
+    "barth2024": {
+      "type": [
+        "UX Study + Prediction System"
+      ],
+      "method": [
+        "UX Study + Prediction System"
+      ],
+      "evaluation": [
+        "User Study"
+      ],
+      "tags": [
+        "prediction",
+        "diabetes",
+        "interface",
+        "UX"
+      ]
+    },
+    "miotto2016": {
+      "type": [
+        "Deep Learning (Autoencoder)"
+      ],
+      "method": [
+        "Deep Learning (Autoencoder)"
+      ],
+      "evaluation": [
+        "Quantitative (ROC, AUC)"
+      ],
+      "tags": [
+        "EHR",
+        "prediction",
+        "autoencoder"
+      ]
+    },
+    "esposito2009": {
+      "type": [
+        "RCT"
+      ],
+      "method": [
+        "RCT"
+      ],
+      "evaluation": [
+        "Survival Analysis (Kaplan-Meier)"
+      ],
+      "tags": [
+        "diet",
+        "Mediterranean",
+        "diabetes",
+        "intervention"
+      ]
+    },
+    "dinu2017": {
+      "type": [
+        "Meta-analysis"
+      ],
+      "method": [
+        "Meta-analysis"
+      ],
+      "evaluation": [
+        "Quantitative (Forest Plot)"
+      ],
+      "tags": [
+        "vegetarian",
+        "chronic disease",
+        "health outcomes"
+      ]
+    }
   }
-];
+};
