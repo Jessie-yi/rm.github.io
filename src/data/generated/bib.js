bc6bf51517306cb65abdb34e2bd0cb799c0e2d53
Jessie-yi <may13466680960@163.com>
2025-05-16 12:23:48 +0100

    Add files via upload

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,4 +1,34 @@
 ﻿const generatedBibEntries = {
+    "barth2024": {
+        "author": "C.-M. Barth and J. Bernard and E. M. Huang",
+        "booktitle": "Proceedings of the ACM on Human-Computer Interaction (CSCW)",
+        "doi": "10.1145/3613904.3642234",
+        "title": "Exploring the Role of Blood Glucose Prediction Technologies for Type 1 Diabetes Self-Management",
+        "type": "inproceedings",
+        "year": "2024"
+    },
+    "dinu2017": {
+        "author": "M. Dinu and R. Abbate and G. F. Gensini and A. Casini and F. Sofi",
+        "doi": "10.1080/10408398.2016.1138447",
+        "journal": "Critical Reviews in Food Science and Nutrition",
+        "number": "17",
+        "pages": "3640--3649",
+        "title": "Vegetarian, Vegan Diets and Multiple Health Outcomes: A Systematic Review with Meta-analysis of Observational Studies",
+        "type": "article",
+        "volume": "57",
+        "year": "2017"
+    },
+    "esposito2009": {
+        "author": "K. Esposito and et al.",
+        "doi": "10.7326/0003-4819-151-5-200909010-00004",
+        "journal": "Annals of Internal Medicine",
+        "number": "5",
+        "pages": "306--314",
+        "title": "Effects of a Mediterranean-style Diet on the Need for Antihyperglycemic Drug Therapy in Patients with Newly Diagnosed Type 2 Diabetes",
+        "type": "article",
+        "volume": "151",
+        "year": "2009"
+    },
     "liu2022": {
         "author": "X. Liu and M. Alharbi and J. Chen and A. Diehl and E. E. Firat and D. Rees and Q. Wang and R. S. Laramee",
         "doi": "10.1177/14738716221126992",
@@ -10,12 +40,64 @@
         "volume": "22",
         "year": "2022"
     },
+    "mcnabb2017": {
+        "author": "L. McNabb and R. S. Laramee",
+        "doi": "10.1111/cgf.13212",
+        "journal": "Computer Graphics Forum",
+        "number": "3",
+        "pages": "589--612",
+        "title": "Survey of Surveys (SoS)",
+        "type": "article",
+        "volume": "36",
+        "year": "2017"
+    },
+    "miotto2016": {
+        "author": "R. Miotto and L. Li and B. A. Kidd and J. T. Dudley",
+        "doi": "10.1038/srep26094",
+        "journal": "Scientific Reports",
+        "pages": "26094",
+        "title": "Deep Patient: An Unsupervised Representation to Predict the Future of Patients from the Electronic Health Records",
+        "type": "article",
+        "volume": "6",
+        "year": "2016"
+    },
     "rees2019": {
         "author": "D. Rees and R. S. Laramee",
         "doi": "10.1111/cgf.13595",
         "journal": "Computer Graphics Forum",
+        "number": "3",
+        "pages": "561--581",
         "title": "A Survey of Information Visualization Books",
         "type": "article",
+        "volume": "38",
         "year": "2019"
+    },
+    "scimone2024": {
+        "author": "A. Scimone and K. Eckelt and M. Streit and A. Hinterreiter",
+        "doi": "10.1109/TVCG.2023.3326936",
+        "journal": "IEEE Transactions on Visualization and Computer Graphics",
+        "title": "Marjorie: Visualizing Type 1 Diabetes Data to Support Pattern Exploration",
+        "type": "article",
+        "year": "2024"
+    },
+    "tong2017": {
+        "author": "C. Tong and R. Roberts and R. S. Laramee and D. Thayer and D. Berridge",
+        "booktitle": "Computer Graphics and Visual Computing (CGVC)",
+        "doi": "10.2312/cgvc.20171276",
+        "pages": "61--70",
+        "title": "Cartographic Treemaps for the Visualization of Public Healthcare Data",
+        "type": "inproceedings",
+        "year": "2017"
+    },
+    "wang2022": {
+        "author": "Q. Wang and R. S. Laramee",
+        "doi": "10.1111/cgf.14424",
+        "journal": "Computer Graphics Forum",
+        "number": "3",
+        "pages": "487--510",
+        "title": "EHR STAR: The State-of-the-Art in Interactive EHR Visualization",
+        "type": "article",
+        "volume": "41",
+        "year": "2022"
     }
 };
